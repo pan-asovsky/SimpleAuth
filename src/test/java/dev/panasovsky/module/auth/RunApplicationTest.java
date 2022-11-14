8a6dbf0e88872bb1dc73d555d8a03b750881a468
pan-asovsky <panosovski@gmail.com>
2022-11-14 15:25:59 +0000

    added the jacoco library

---
+++ src/test/java/dev/panasovsky/module/auth/RunApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.panasovsky.module.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class RunApplicationTest {
+
+    @Test
+    void contextLoads() {}
+
+}
