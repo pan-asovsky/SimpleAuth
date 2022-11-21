9b73120d104076af343ef58eb1448ea7a013fbdb
pan-asovsky <panosovski@gmail.com>
2022-11-21 16:42:32 +0000

    the user registration method has been moved to the UserService

--- src/main/java/dev/panasovsky/module/auth/services/UserService.java
+++ src/main/java/dev/panasovsky/module/auth/services/UserService.java
@@ -4,6 +4,7 @@ import dev.panasovsky.module.auth.entities.User;
 import dev.panasovsky.module.auth.repositories.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,7 +25,14 @@ public class UserService implements UserDetailsService {
         return new UserDetailsPrincipal(user);
     }
 
-    public User addUser(final User user) {
+    public String registerUser(final User user) {
+
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        final User addUser = addUser(user);
+        return "Successfully registered " + addUser.getLogin();
+    }
+
+    private User addUser(final User user) {
         return userRepository.save(user);
     }
 
